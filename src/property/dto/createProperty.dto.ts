63f6088176f68f91dc080f6badbaf63fc0af009b
Yogurt7v <egorov-2k@yandex.ru>
2025-02-10 20:08:16 +0300

    проверка значений body при помощи функции new ValidationPipe()

---
+++ src/property/dto/createProperty.dto.ts
@@ -0,0 +1,14 @@
+import { IsNumber, IsPositive, IsString, Length } from "class-validator";
+
+export class CreatePropertyDto {
+    @IsString()
+    @Length(2, 30)
+    name: string;
+
+    @IsString()
+    description: string;
+
+    @IsNumber() // функция из класс-валидатора для валидации типа значения
+    @IsPositive()
+    area: number
+}
--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post, Param, Body, HttpCode } from '@nestjs/common';
+import { Controller, Get, Post, Param, Body, HttpCode, ParseIntPipe, Query, ParseBoolPipe, UsePipes, ValidationPipe, Patch } from '@nestjs/common';
+import { CreatePropertyDto } from './dto/createProperty.dto';
 
 @Controller('property')
 export class PropertyController {
@@ -8,13 +9,27 @@ export class PropertyController {
         return "ALL properties";
     }
 
-    @Get(':id/:slug')
-    findOne(@Param('id') id, @Param("slug") slug) {
-        return { id: id, slug: slug };
+    @Get(':id')
+    findOne(@Param('id', ParseIntPipe) id, @Query("sort", ParseBoolPipe) sort) {
+        return { id, sort };
     }
     @Post()
-    @HttpCode(202) // так можно менять код ответа
-    create(@Body("test") test) {
-        return test; // так вытаскивается значение из объекта если есть поле "тест"
+    // @HttpCode(202) // так можно менять код ответа
+    // create(@Body("test") test) {
+    //     return test; // так вытаскивается значение из объекта если есть поле "тест"
+    // }
+    @UsePipes(new ValidationPipe({ whitelist: true, groups: ['create'] }))
+    // выбираем эту функцию для запуска валидации согласно типам CreatePropertyDto и только им. лишнее будет удалено
+    create(@Body() body: CreatePropertyDto) {
+        console.log(body)
+        return body
+    }
+
+    @Patch(':id')
+    update(@Body(new ValidationPipe({
+        whitelist: true,
+        groups: ['update']
+    })) body: CreatePropertyDto) {
+        return body
     }
 }
