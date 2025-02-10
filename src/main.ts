8a32ef1c2d5b1186c5007fbee0cf6753c1ab1a17
Yogurt7v <egorov-2k@yandex.ru>
2025-02-10 20:37:47 +0300

    проверил валидацию через ParseIdPipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // глобальная проверка валидации, может отдельно проверяться в к контроллере и в модуле, или здесь
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }))
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, Post, Param, Body, HttpCode, ParseIntPipe, Query, ParseBoolPipe, UsePipes, ValidationPipe, Patch } from '@nestjs/common';
 import { CreatePropertyDto } from './dto/createProperty.dto';
+import { IdParamDto } from './dto/idParam.dto';
+import { ParseIdPipe } from './pipes/parseIdppipes';
 
 @Controller('property')
 export class PropertyController {
@@ -11,25 +13,30 @@ export class PropertyController {
 
     @Get(':id')
     findOne(@Param('id', ParseIntPipe) id, @Query("sort", ParseBoolPipe) sort) {
-        return { id, sort };
+
+        console.log(id)
+        console.log(sort)
+
+        return id;
     }
     @Post()
     // @HttpCode(202) // так можно менять код ответа
+
     // create(@Body("test") test) {
     //     return test; // так вытаскивается значение из объекта если есть поле "тест"
     // }
-    @UsePipes(new ValidationPipe({ whitelist: true, groups: ['create'] }))
+
+    // @UsePipes(new ValidationPipe({ whitelist: true, groups: ['create'] }))
     // выбираем эту функцию для запуска валидации согласно типам CreatePropertyDto и только им. лишнее будет удалено
+    // но если мы проверяем ее глобально в main, то здесь она не нужна
+
     create(@Body() body: CreatePropertyDto) {
         console.log(body)
         return body
     }
 
     @Patch(':id')
-    update(@Body(new ValidationPipe({
-        whitelist: true,
-        groups: ['update']
-    })) body: CreatePropertyDto) {
+    update(@Param("id", ParseIdPipe) id, @Body() body: CreatePropertyDto) {
         return body
     }
 }
