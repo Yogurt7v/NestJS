b89422199ba88796e5123cfcd36623d754173c6d
Yogurt7v <egorov-2k@yandex.ru>
2025-02-11 21:23:19 +0300

    some fix

--- dbConfig.ts
+++ dbConfig.ts
@@ -5,10 +5,10 @@ export const pgConfig: PostgresConnectionOptions = {
     url: "postgresql://yogurt7v:xxFOI7bvIhmooLqT70vx2QdZDknHLY6T@dpg-culoqolsvqrc73chqe9g-a.oregon-postgres.render.com/realestatedb_wzap",
     type: "postgres",
     port: 5432,
-    entities: [Property],
+    entities: [__dirname + "/**/*.entity{.ts,.js}"], // добавлены все сущности из данной папки чтобы не заводить руками
     synchronize: true, // только для разработки, НЕ ДЛЯ ПРОДАКШЕНА!!!
     ssl: {
-        rejectUnauthorized: false, // для самоподписанных сертификатов
+        rejectUnauthorized: false,
     },
 }
 // убрать потом в .env
