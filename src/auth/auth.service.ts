9121b89c0cef8396dfe89c5db2cee009acd6f007
Yogurt7v <egorov-2k@yandex.ru>
2025-02-20 20:47:03 +0300

    отправляем на localhost:3000/auth/login     "email": "adam@god.com",     "password": "123"
        и получаем статус 200. аутентификацию проходит

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { compare } from 'bcrypt';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(private userService: UserService) { }
+
+    async validateUser(email: string, password: string) {
+        const user = await this.userService.findByEmail(email)
+
+        if (!user) {
+            throw new UnauthorizedException("Пользователь не найден.")
+        }
+        const isPasswordMatch = await compare(password, user.password) // функция bcrypt сравнивает наш пароль и пароль из запроса
+
+        if (!isPasswordMatch) {
+            throw new UnauthorizedException("Неверный пароль")
+        }
+
+        return { id: user.id } // возвращать весь объект плохо. там лишние данные и те которые не надо показывать
+    }
+
+}
