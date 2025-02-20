01c29aa49881a8e033da96cf532e00bd972fe5a9
Yogurt7v <egorov-2k@yandex.ru>
2025-02-20 20:53:07 +0300

    сделана своя localStrategy

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthGuard } from '@nestjs/passport';
+// import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './guards/local-auth/local-auth.guard';
 
-@Controller('auth')
+@Controller('auth') // адрес получается auth/login
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
   @HttpCode(HttpStatus.OK)
-  @UseGuards(AuthGuard("local"))
+  @UseGuards(LocalAuthGuard) // активирует локальную стратегию
   @Post("login")
   async login(@Request() req) {
     return req.user
