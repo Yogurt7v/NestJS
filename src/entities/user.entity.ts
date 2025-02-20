bcd261830f4fa7985675174baa879b84597a4305
Yogurt7v <egorov-2k@yandex.ru>
2025-02-20 19:49:53 +0300

    add bcrypt. некоторые зависимости не поддерживаются

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,4 +1,4 @@
-import { Column, CreateDateColumn, Entity, JoinTable, ManyToMany, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, Column, CreateDateColumn, Entity, JoinTable, ManyToMany, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Property } from "./property.entity";
 
 @Entity()
@@ -22,10 +22,18 @@ export class User {
     @CreateDateColumn() // сразу сгенерирует время при создании
     createdAt: Date
 
+    @Column()
+    password: string
+
     @OneToMany(() => Property, (property) => property.user)
     properties: Property[]
 
     @ManyToMany(() => Property, (property) => property.likedBy)
     @JoinTable({ name: "user_liked_properties" }) // объединяет данные в таблицу которую мы называем user_liked_properties
     likedProperties: Property[]
+
+    @BeforeInsert()  // эта функция будет хэшировать пароль с помощью bcrypt перед вставкой в базу данных
+    hashPassword() {
+
+    }
 }
